85d21cfe960f6f32f410d9fc0a7b63047a911e95
Haroon Ahmad <haroon334@gmail.com>
2021-10-27 11:53:11 +0200

    dates

--- operators.php
+++ operators.php
@@ -1,3 +1,6 @@
+<!-- # Use of Math Operators! -->
+
+
 <?php 
 
 $x = 6;
@@ -7,6 +10,36 @@ echo $x+$y,
       $x-$y,
       $x*$y,
       $x/$y % 10
+
+
+?>
+<br>
+<!-- # Use of Comparison Operators ! -->
+<?php
+$i = 10;
+$h = 15;
+
+echo $i < $h;
+
+    var_dump($i == $h);
+    var_dump($i > $h);
+    var_dump($i != $h);
+    var_dump($i <= $h);
+
+?>
+
+<?php 
+
+if ($i < $h || $i == $h){
+
+    echo "<h3>This is comparison </h3>";
+
+}
+
+if (!($i > $h)){
+
+    echo "<h4>This is Not</h4>";
+};
 
 
 ?>
